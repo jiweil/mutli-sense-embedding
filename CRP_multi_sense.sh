04e0c08c9a052d838343065158d68f33bd1b2800
Jiwei li <bdlijiwei@john1.stanford.edu>
2015-11-24 10:15:47 -0800

    sdf

--- CRP_multi_sense.sh
+++
@@ -1,2 +0,0 @@
-javac CRP_multi_sense.java
-java CRP_multi_sense -dimension 300 -word_window 15 -save_file CRP -frequency_file frequency.txt -train_file small.txt -load_embedding 0 -embedding_file small_vect
--- CRP_multi_sense_multi.sh
+++
@@ -1,3 +0,0 @@
-javac CRP_multi_sense.java
-java CRP_multi_sense -dimension 300 -word_window 15 -save_file this_CRP -frequency_file ../../Wiki_Data/release/frequency.txt -train_file ../../Wiki_Data/release/train_file1.txt -load_embedding 1 -embedding_file ../../Wiki_Data/release/all/small_vect
-#java CRP_multi_sense_multi1 -dimension 300 -word_window 15 -save_file CRP -frequency_file ../../Wiki_Data/release/frequency.txt -train_file ../../Wiki_Data/release/train_file1.txt -load_embedding 1 -embedding_file ../../Wiki_Data/release/all/small_vect
